6544fb3ef23694470a4faf373933ea04c52c01b3
Divya <divyadc.01@gmail.com>
2021-04-05 23:15:45 -0700

    Adding heroku server link

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ import "./FaceRecognition.css";
 const FaceRecognition = ({identifyBox, imageUrl}) => {
     return(
         <div className='center ma'>
-            <div className= 'absolute-mt2'>
+            <div className= 'absolute mt2'>
                 <img id = 'inputImage' alt = '' src = {imageUrl} width = '500px' height='auto'/>
                 <div className='bounding-box' style = {{top: identifyBox.topRow, right: identifyBox.rightCol, bottom: identifyBox.bottomRow, left: identifyBox.leftCol}}> </div>
             </div>
